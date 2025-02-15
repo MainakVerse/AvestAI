e6a7a9e31b7d779c4af3e5cb6815bacc827592dd
MainakVerse <mainakchaudhuri671@gmail.com>
2025-02-15 20:44:16 +0530

    Committed

---
+++ config/site.tsx
@@ -0,0 +1,32 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Avest-AI",
+  title: "Avest-AI",
+  description:
+    "Avest-AI is a startup that provides AI solutions for businesses and individuals.",
+  mainNav: [
+    {
+      title: "Docs",
+      href: "/docs",
+    },
+    {
+      title: "Pricing",
+      href: "/pricing",
+    },
+    {
+      title: "Newsletter",
+      href: "/newsletter",
+    },
+    {
+      title: "Explore",
+      href: "/browse",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/iambornmainak",
+    github: "https://github.com/MainakVerse",
+    docs: "https://github.com/MainakVerse",
+    api: "https://api.Avest-AI.io",
+  },
+};
